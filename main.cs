25d771c20ae49d2cd1554a1b6360357cf9b15935
Creator125OCA <85368990+Creator125@users.noreply.github.com>
2021-06-16 20:59:18 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+   // Console.WriteLine ("Hello World");
+
+  int suma = 0;
+  int num = 5;
+
+  num 
+    for (int i=1; i<=12; i++){
+      suma = num +i;
+    }
+
+    Console.WriteLine(suma);
+
+  
+  }
+}
